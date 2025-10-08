69d1403ad8098ccfd82e0a62afe61e9814730dcb
Packer Barlow <packerb2@byu.edu>
2025-10-08 10:33:22 -0600

    added jsx files for each page

---
+++ src/analysis/analysis.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Analysis() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>analysis displayed here</div>
+    </main>
+  );
+}
---
+++ src/calander/calander.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Calander() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>calander displayed here</div>
+    </main>
+  );
+}
