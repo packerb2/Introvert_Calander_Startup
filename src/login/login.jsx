0b4448066ee89b529cd2233690b1285ef77257fa
Packer Barlow <packerb2@byu.edu>
2025-10-08 18:24:38 -0600

    converted login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+      <div>
+          <h1>Welcome to your</h1>
+          <h2>
+              <font size="7">Spoon-Saving Calander</font>
+          </h2>
+          <form method="get" action="calander.html">
+              <div className="input-group mb-3">
+                  <span className="input-group-text">Username: </span>
+                  <input className="form-control" type="text" placeholder="Enter Username Here" />
+              </div>
+              <div className="input-group mb-3">
+                  <span className="input-group-text">Password: </span>
+                  <input className="form-control" type="password" placeholder="Enter Password Here" />
+              </div>
+              <button type="submit" className="btn btn-primary">Login</button>
+              <button type="submit" className="btn btn-secondary">Create Account</button>
+          </form>
+      </div>
     </main>
   );
 }
