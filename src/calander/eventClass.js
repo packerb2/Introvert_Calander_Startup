c64cad30254e8613c704d2c17718d8c18721c539
Packer Barlow <packerb2@byu.edu>
2025-10-24 13:42:40 -0600

    got rid of un-needed file

--- src/calander/eventClass.js
+++
@@ -1,9 +0,0 @@
-class myEventClass {
-    constructor(eventName, eventDate, eventTime, eventPeople, eventSpoons) {
-        this.eventName = eventName;
-        this.eventDate = eventDate;
-        this.eventTime = eventTime;
-        this.eventPeople = eventPeople;
-        this.eventSpoons = eventSpoons;
-    }
-}
